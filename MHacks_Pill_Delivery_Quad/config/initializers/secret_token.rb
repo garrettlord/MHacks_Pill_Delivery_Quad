b042e06b77204606164b2e09b922bd919ebd4a5e
Garrett Lord <garrettlord@gmail.com>
2013-09-22 00:44:45 -0400

    fresh rails project

---
+++ MHacks_Pill_Delivery_Quad/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MHacksPillDeliveryQuad::Application.config.secret_key_base = '29af6f491854cf37f86598012249ade640dbaa28c8b2b8238c2303413e1a87bded0d7741b85e91bd8cee0237ef85ee70d47cc8384657c433fb5be4068508fc36'
