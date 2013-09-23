f3b711487fe3368a5fc0e5ba3e85c79f26591e63
Garrett Lord <garrettlord@gmail.com>
2013-09-23 08:38:49 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+hospitals = Hospital.create([{ name: 'UN dispensary', latitude: 35.93, longitude: 35.92}])
+users = User.create([{ hospital_id: 1, name: "Bob George", phone_number: "123-456-7890", priveledge: 1},
+					 { hospital_id: 1, name: "Steve Kowen", phone_number: "325-382-2845", priveledge: 2},
+					 { hospital_id: 1, name: "Mike Wizowski", phone_number: "839-942-1938", priveledge: 3}])
+copters = Copter.create([{ hospital_id: 1, name: "refugee deliveries", category: "quadcopter", last_latitude: 35.93, last_longitude: 35.92, available: true}])
+medicines = Medicine.create([{ hospital_id: 1, name: "aspirin", category: "painkiller", description: "reduces pain and fevers", restricted: false, quantity: 100}])
+destinations = Destination.create([{ hospital_id: 1, name: "outpost", description: "on the outskirts of a refugee camp", latitude: 36.0, longitude: 35.90}])
